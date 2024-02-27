9ce4292db98d973d2ba6064a49d246be57797bb2
Nicocahya <nr016082@gmail.com>
2024-02-27 15:47:47 +0700

    belajar react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    
+      <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+        <h1>Hello world!</h1>
+      </div>
+      
+    
+  )
+}
+
+export default App
