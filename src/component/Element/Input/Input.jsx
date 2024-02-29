b347acc92fecc38edd35f209e1aec6a2675a62dd
Nicocahya <nr016082@gmail.com>
2024-02-29 09:22:37 +0700

    React Router

--- src/component/Element/Input/Input.jsx
+++ src/component/Element/Input/Input.jsx
@@ -6,6 +6,7 @@ return (
     className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder: oppacity-50"
     placeholder={placeholder} 
     name={name}
+    id={name}
     />
 );
 };
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,15 @@
 import AuthLayout from "../component/LayOut/AuthLayouts";
 import FormLogin from "../component/Fragment/FormLogin";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
     return (
         <AuthLayout title = "Login">
             <FormLogin/>
+            <p className="text-sm mt-7 text-center">
+                Don't have an account? {" "} 
+                <Link to="/register" className="font-bold text-blue-600">Register</Link>
+            </p>
         </AuthLayout>
     );
 };
