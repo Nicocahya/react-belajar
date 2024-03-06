df01c7fd54b508b24fc13d21bdafd5ce915ab571
Nicocahya <nr016082@gmail.com>
2024-03-06 10:40:52 +0700

    get data api

--- src/component/Fragment/CardProduct.jsx
+++ src/component/Fragment/CardProduct.jsx
@@ -5,7 +5,7 @@ import Button from "../Element/Button";
 const CartProduct = (props) => {
     const {children} = props;
     return (
-            <div className="w-full max-w-ws bg-gray-700 border border-gray-600 rounded-lg shadow mx-3 my-2 flex flex-col justify-between">
+            <div className="w-full max-w-64 bg-gray-700 border border-gray-600 rounded-lg shadow mx-5 my-2 flex flex-col justify-between">
                 {children}
             </div>
     );       
@@ -15,10 +15,7 @@ const Header = (props) => {
     const {image} = props;
     return (
         <a href="#">
-            <img 
-            src={image} 
-            alt="product" 
-            className="p-8 rounded-t-lg" />
+            <img src={image} alt="product" className="p-8 rounded-t-lg h-60 w-full object-cover" />
         </a>
     );
 };
@@ -28,12 +25,8 @@ const Body = (props) => {
     return (
         <div className="px-5 pb-5 h-full">
             <a href="">
-                <h5 className="text-xl font-semibold tracking-tight text-white">
-                    {name}
-                </h5>
-                <p className="text-m text-white">
-                    {children}
-                </p>
+            <h3 className="text-xl font-semibold tracking-tight text-white">{name.substring(0, 20)}...</h3>
+                <p className="text-xs text-white">{children.substring(0, 100)}...</p>
             </a>
         </div>
     );
@@ -43,8 +36,8 @@ const Footer = (props) => {
     const {price, handleAddToCart, id } = props;
     return (
         <div className="flex items-center justify-between px-5 pb-5">
-            <span className="text-xm font-bold text-white"> Rp{" "} {price.toLocaleString('id-ID', {styles:'currency', currency: 'IDR'})}</span>
-            <Button classname="bg-blue-600 text-xs " onClick={() =>handleAddToCart(id)}>Add to Card</Button>
+            <span className="text-xl font-bold text-white">$ {price.toLocaleString('id-ID', {styles: 'currency', currency: 'USD'})}</span>
+            <Button classname="bg-teal-500 text-base" onClick={() => handleAddToCart(id)}>Add to cart</Button>
         </div>
     );
 };
--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -2,33 +2,34 @@ import { Fragment, useEffect, useRef, useState } from "react";
 import Button from "../component/Element/Button";
 import CartProduct from "../component/Fragment/CardProduct";
 import Counter from "../component/Fragment/Counter";
+import { getProducts } from "../services/product.service";
 
-const products = [
-    {
-        id: 1,
-        name: "Sepatu Baru",
-        price: 1900000,
-        image: "/image/sepatu1.jpg",
-        description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-        Quibusdam, expedita? Lorem ipsum dolor sit amet sadam alok terizla lunox epep emel`
-    },
+// const products = [
+//     {
+//         id: 1,
+//         name: "Sepatu Baru",
+//         price: 1900000,
+//         image: "/image/sepatu1.jpg",
+//         description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+//         Quibusdam, expedita? Lorem ipsum dolor sit amet sadam alok terizla lunox epep emel`
+//     },
 
-    {
-        id: 2,
-        name: "Sepatu Lawas",
-        price: 1200000,
-        image: "/image/sepatu1.jpg",
-        description: `Quibusdam, expedita? Lorem ipsum dolor sit amet sadam alok terizla lunox epep emel`
-    },
+//     {
+//         id: 2,
+//         name: "Sepatu Lawas",
+//         price: 1200000,
+//         image: "/image/sepatu1.jpg",
+//         description: `Quibusdam, expedita? Lorem ipsum dolor sit amet sadam alok terizla lunox epep emel`
+//     },
 
-    {
-        id: 3,
-        name: "Sepatu Import",
-        price: 3000000,
-        image: "/image/sepatu1.jpg",
-        description: `Quibusdam, expedita? Lorem ipsum dolor sit amet sadam alok terizla lunox epep emel`
-    }
-]
+//     {
+//         id: 3,
+//         name: "Sepatu Import",
+//         price: 3000000,
+//         image: "/image/sepatu1.jpg",
+//         description: `Quibusdam, expedita? Lorem ipsum dolor sit amet sadam alok terizla lunox epep emel`
+//     }
+// ]
 
 const email = localStorage.getItem("email");
 
@@ -36,12 +37,27 @@ const ProductsPage = () => {
 
     const [cart, setCart] = useState([]);
     const [totalPrice, setTotalPrice] = useState (0);
+    const [products, setProducts] = useState([]);
+
     useEffect (() => {
         setCart (JSON.parse(localStorage.getItem("cart")) || []);
     }, []);
 
+    useEffect(() => {
+        getProducts((data) => {
+            console.log(data)
+        });
+    }, [])
+
     useEffect (() => {
-        if(cart.length > 0){
+                getProducts((data) => {
+            setProducts(data);
+        });
+    });
+
+    useEffect(() => {
+        if(products.length > 0 &&
+            cart.length > 0) {
             const sum = cart.reduce ((acc, item) => {
                 const product = products.find((product) => product.id === item.id);
                 return acc + product.price * item.qty;
@@ -49,7 +65,7 @@ const ProductsPage = () => {
             setTotalPrice(sum);
             localStorage.setItem ("cart", JSON.stringify(cart));
         }
-    }, [cart])
+    }, [cart, products]);
 
     const handleLogout = () => {
         localStorage.removeItem('email');
@@ -91,16 +107,17 @@ const ProductsPage = () => {
                 <Button classname=" ml-5 bg-black" onClick={handleLogout}>Log Out</Button>
             </div>
             <div className="flex justify-center py-5">
-                <div className="w-4/6 flex flex-w">
-                    {products.map((product) => (
+                <div className="w-4/6 flex flex-wrap">
+                {products.length > 0 && 
+                products.map((product) => (
                         <CartProduct key={product.id}>
                             <CartProduct.Header image={product.image} />
-                            <CartProduct.Body name={product.name}>
+                            <CartProduct.Body name={product.title}>
                                 {product.description}
                             </CartProduct.Body>
                             <CartProduct.Footer price={product.price}
                                 id={product.id}
-                                handleAddToCart={handleAddToCartRef} />
+                                handleAddToCart={handleAddToCart} />
                         </CartProduct>
                     ))}
                 </div>
@@ -121,21 +138,21 @@ const ProductsPage = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {cartRef.current.map((item) => {
+                        {products.length > 0 && 
+                        cart.map((item) => {
                                 const product = products.find (
                                     (product) => product.id === item.id
                                     );
                                     return (
                                         <tr key={item.id}>
-                                            <td>{product.name}</td>
-                                            <td>
-                                            Rp{" "} 
-                                            {product.price.toLocaleString('id-ID', {styles:'currency', currency: 'IDR'})}
+                                            <td>{product.title.substring(0, 10)}...</td>
+                                            <td>$ 
+                                                {product.price.toLocaleString('id-ID', {styles: 'currency', currency: 'USD'})}
                                             </td>
                                             <td>{item.qty}</td>
-                                            <td> Rp {(item.qty * product.price).toLocaleString ('id-ID',
+                                            <td> $ {(item.qty * product.price).toLocaleString ('id-ID',
                                              {styles:'currency', 
-                                             currency: 'IDR'
+                                             currency: 'USD'
                                              })}
                                              </td>
                                         </tr>
@@ -147,10 +164,10 @@ const ProductsPage = () => {
                                 </td>
                                 <td>
                                 <b>
-                                 Rp {" "}    
+                                 $ {" "}    
                                 {totalPrice.toLocaleString
                                 ('id-ID', 
-                                {styles:'currency', currency: 'IDR'}
+                                {styles:'currency', currency: 'USD'}
                                 )} 
                                 </b>
                                 </td>
